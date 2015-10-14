3fd4606cdb5b5a52cc09700a5e150b4fdb3ce354
antonwass <anton_wass@hotmail.com>
2015-10-14 12:28:48 +0200

    Test

--- src/zombiedolphins/ZombieDolphins.java
+++ src/zombiedolphins/ZombieDolphins.java
@@ -27,7 +27,7 @@ public class ZombieDolphins extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello World! /Anton");
             }
         });
         
